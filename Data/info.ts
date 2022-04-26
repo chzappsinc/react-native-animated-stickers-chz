ee3be155d9f721848788b986a1ab36630429727f
Jasim Tk <65512279+chzappsinc@users.noreply.github.com>
2022-04-27 02:01:12 +0530

    Update 2.0.1

--- Data/info.ts
+++
@@ -1,7 +0,0 @@
-export default {
-    version : '1.202.22',
-    total_files : '12',
-    format : '@chzapps/sticker/@render/',
-    item_end : '.sticker.no.start.apps/auto/false/size=userPref,{render=true}/@data/ims',
-    update : 'https://sticker.chzapps.com/update.json'
-}
