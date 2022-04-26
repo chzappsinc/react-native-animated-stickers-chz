7d782837b1c4f29cc9e6ea2ab94cd1896c1c55fb
Jasim Tk <65512279+chzappsinc@users.noreply.github.com>
2022-04-27 02:03:44 +0530

    2.0.1

---
+++ files/Data/fonts.ts
@@ -0,0 +1,6 @@
+export default {
+    title : 'Tourney-Regular',
+    bold : 'Dosis-Medium',
+    medium : 'Dosis-Regular',
+    light : 'Dosis-Light'
+}
---
+++ files/Items/R/dim.ts
@@ -0,0 +1,7 @@
+export default {
+    long_press :  '<b>① Long Press</b>\n Long press on sticker to get it\'s animated view and details of sticker\n',
+    render_android : '<b>② Swipe!</b>\nSwipe your sticker anywhere to send it!! <i>Cool na?</i>\nWork\'s on both android and IOS\nYou can enable it on setting\'s if it\'s not available\n',
+    render_ios : '<b>② Render</b>\nHurr you are using an IOS device! <i>cool </i>\nYour sticker\'s may download each time as you view , We cannot ensure cache in ios platform\n',
+    data : '<b>③ Data Source</b>\nAll sticker\'s are copyrighted by it\'s owner\nYou can follow them on their social media or visit their website will make them more happy\n',
+    agree : '<b>④ Usage</b>\nAll custom sticker\'s are build by <b>%s% </b> & Sticker\'s are rendered by <b> %m% </b>',
+}
